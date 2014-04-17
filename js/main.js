4cdfcfd066c7e854756598275a08c71c5be01934
kaifius <kai.hofius@gmail.com>
2014-04-16 23:29:46 -0700

    disable js for mobile

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 $(function() {
-  $(window).bind('scroll', function(){
-    var navHeight = $(window).height() - 70;
+  $(window).resize(function(){
+    if ($(this)).width() > 480){
+        $(window).bind('scroll', function(){
+          var navHeight = $(window).height() - 70;
 
-    if ( $(window).scrollTop() > navHeight ) {
-      $('nav').addClass('fixed');
-    } else {
-      $('nav').removeClass('fixed');
+          if ( $(window).scrollTop() > navHeight ) {
+            $('nav').addClass('fixed');
+          } else {
+            $('nav').removeClass('fixed');
+          }
+
+        });
+
+        $('nav a').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            // target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                scrollTop: target.offset().top
+              }, 500);
+              return false;
+            }
+          }
+        });
     }
-
   });
 
-  $('nav a').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      // target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 500);
-        return false;
-      }
-    }
-  });
-  
 });
